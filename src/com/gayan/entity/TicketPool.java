de489bbf63859a25d2ea71063e1caf48569477f7
Gayan Kalinga <bg15407@gmail.com>
2025-04-27 11:30:14 +0530

    Ticket Pool Interface done

---
+++ src/com/gayan/entity/TicketPool.java
@@ -0,0 +1,21 @@
+package com.gayan.entity;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface TicketPool {
+    void addTicket(Ticket ticket); //vendors/producers to add ticket
+    Optional<Ticket> purchaseTicket(); //consumers to purchase ticket
+    int getCurrentSize(); //readers to query the current size
+    int getCapacity(); //get the max no of tickets the pool can hold
+    int getAvailableTicketCount(); // readers to check how many unsold tickets are available
+    int getSoldTicketCount(); //readers to check how many tickets are sold.
+    List<Ticket> getAllTickets(); //return list of all tickets in the pool
+    void printTicketPoolStatus();
+    Ticket createTicket(
+            String eventName,
+            String vendorName,
+            String location,
+            double price);
+    void cancelTicket(Ticket ticket);
+}
